2c7a73b6ff3c352e8e6e8e3384abc2bd7bb6f1e3
prathameshpatkar <48145449+prathameshpatkar@users.noreply.github.com>
2019-05-17 11:17:15 +0530

    Add files via upload

---
+++ Task 8.ps1
@@ -0,0 +1,26 @@
+﻿Get-WmiObject win32_operatingsystem -ComputerName MUM-PC09AKTC | Select-Object lastbootuptime
+
+get-help Get-WmiObject -Online
+
+Get-WmiObject -list
+
+Get-WmiObject NTEventlogProviderConfig -ComputerName MUM-PC09AKTC | Select-Object lastbootuptime
+
+Get-WmiObject win32_operatingsystem | Get-Member
+
+Get-WmiObject 
+
+function compstatus
+{
+param($compn)
+Get-WmiObject win32_operatingsystem -ComputerName $compn | Select-Object lastbootuptime
+
+ Get-WmiObject win32_computersystem -ComputerName $compn | Select-Object -Property name
+ $env:COMPUTERNAME
+ }
+
+compstatus -compn MUM-PC09AKTC
+
+Get-WmiObject win32_computersystem | get-member
+
+ Get-WmiObject win32_computersystem -ComputerName MUM-PC09AKTC  | Select-Object -Property name
