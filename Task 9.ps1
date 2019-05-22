18741d00270199dff551b4e4e5996f2556ebaac0
prathameshpatkar <48145449+prathameshpatkar@users.noreply.github.com>
2019-05-22 18:14:12 +0530

    Add files via upload

---
+++ Task 9.ps1
@@ -0,0 +1,30 @@
+﻿function getasservice
+
+{
+$var=$_
+
+$allservices= Get-WmiObject win32_service
+
+foreach($service in $allservices)
+{
+$myvalue=$service.path
+$myarray=$myvalue.split("\")
+$nameofprocess=$myarray
+if ($var.processname -eq $nameofprocess)
+
+{
+
+$alldata=[PSCustomobject]@{processname=$var.processname;Relevantserice="YES"}
+}
+else
+{
+Write-Host "Relevant service for process hasn't been found"
+}
+
+$alldata | Out-File -FilePath "C:\Users\ppatkar\Desktop\new.csv" -Append
+}
+    
+
+}
+
+Get-Process | getasservice
